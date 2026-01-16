d207c1b53545537b6957324de909dbdf5bb6e950
Miguel Cardenas <miguelcarde.3@gmail.com>
2026-01-16 09:19:35 -0500

    feat: examplo of how to navigate to a different screen

---
+++ app/profile.tsx
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
+
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+
+export default function ProfileScreen() {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">Profile Screen</ThemedText>
+      <ThemedText>This is a flat route (not a modal)</ThemedText>
+      <Link href="/" style={styles.link}>
+        <ThemedText type="link">Go to Home</ThemedText>
+      </Link>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+});
